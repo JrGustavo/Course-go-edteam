3d8779646c61320f211d8be07b4cf7db0afbb428
JrGustavo <gus@soypetal.com>
2024-05-22 22:50:52 -0500

    Update estructures

--- conceptos/estructuras/main.go
+++ conceptos/estructuras/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+type Person struct {
+	Name        string
+	Age         uint8
+	HasChildren bool
+}
+
+func main() {
+	junior := Person{
+		Name:        "Junior",
+		Age:         30,
+		HasChildren: false,
+	}
+
+	fmt.Printf("%+v", junior)
 }
